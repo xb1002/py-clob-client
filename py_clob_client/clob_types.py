70515c014d2a1a29d834eb4595647d00baeb9c0f
xb1002 <127486192+xb1002@users.noreply.github.com>
2026-04-07 15:50:51 +0800

    Allow fee_rate_bps override via PartialCreateOrderOptions

--- py_clob_client/clob_types.py
+++ py_clob_client/clob_types.py
@@ -219,6 +219,7 @@ class CreateOrderOptions:
 class PartialCreateOrderOptions:
     tick_size: Optional[TickSize] = None
     neg_risk: Optional[bool] = None
+    fee_rate_bps: Optional[int] = None
 
 
 @dataclass
